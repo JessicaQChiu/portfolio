4cef2c4e5d01d9b26442d95b355330e5a56033e7
JessicaQChiu <63030134+JessicaQChiu@users.noreply.github.com>
2021-04-22 22:06:13 +1200

    Fixing file uploading error

---
+++ 8/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+$(document).ready(function(){
+    //Make the <header> element stretch across the entire screen*/
+    $('.header').height($(window).height());
+  })
